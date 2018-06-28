43c090780c25faf77fd5db8b3bb513f126c513d5
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2018-06-28 14:29:33 -0300

    grasshopper

--- testeGenerativo3.py
+++ testeGenerativo3.py
@@ -35,8 +35,7 @@ model('basic', '-ndm', 3, '-ndf', ndf)
 Nx = 5
 Ny = 2
 Nz = 2
-Nx=Nx+1
-Ny=Ny+1
+
 
 # Valor dos vãos de viga e distância entre pavimentos
 Dx = 5.
@@ -44,22 +43,15 @@ Dy = 7.
 Dz = 3.
 
 # Definição da função com o Número de nós Npts e índice dos nós (tags) I
-Npts = Nx*Ny*Nz
+Npts = (Nx)*(Ny)*(Nz)
 for I in range(0, Npts, 1):
-    
-    node(I, (I % Nx)*Dx, ((I % (Nx*Ny))//Nx)*Dy, ((I // (Nx*Ny)) + 1)*Dz)
-#    node(int(I[i]), float((I[i] % Nx)*Dx), float(((I[i] % (Nx*Ny))//Nx)*Dy), float(((I[i] // (Nx*Ny)) + 1)*Dz))
+    node(I, (I % Nx)*(Dx), ((I % (Nx*Ny))//Nx)*(Dy), ((I // (Nx*Ny)) + 1)*Dz)
+   
 
 
 #testando se os nós foram criados
 a = nodeCoord(2)
 print(a)
-
-p
-
-
-
-
 
 #Contador de tempo
 dt = timer() - start
